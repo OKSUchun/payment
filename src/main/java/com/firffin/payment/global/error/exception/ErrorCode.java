fd8817684675c24658cd4978600a2a535ed58e27
OKSUchun <oksuchun119@gmail.com>
2024-09-24 13:20:33 +0900

    [CHORE] ErrorCode 정의

--- src/main/java/com/firffin/payment/global/error/exception/ErrorCode.java
+++ src/main/java/com/firffin/payment/global/error/exception/ErrorCode.java
@@ -12,7 +12,14 @@ import static org.springframework.http.HttpStatus.*;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-  SAMPLE_ERROR(BAD_REQUEST, "Sample Error Message");
+    SAMPLE_ERROR(BAD_REQUEST, "Sample Error Message"),
+    SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 에러가 발생하였습니다."),
+
+    DUPLICATE_PHONE_NUMBER(CONFLICT, "이미 존재하는 핸드폰번호입니다."),
+
+    UNREGISTERED_CHILD(BAD_REQUEST, "자녀 유저는 부모 유저가 먼저 등록해야합니다."),
+    INVALID_ROLE(BAD_REQUEST, "유효한 권한이 아닙니다.");
+
 
     /**
      * 오류 HTTP 상태 코드.
